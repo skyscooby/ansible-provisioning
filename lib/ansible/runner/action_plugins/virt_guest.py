903897a86af76d5996cc6b8928be0cfe678b0938
Andrew G <andrew@agrnwd.com>
2013-11-14 02:42:08 -0500

    Update virt_guest.py
    utils module does not import template_from_file any longer and function is not resolved unless addressed directly.

--- lib/ansible/runner/action_plugins/virt_guest.py
+++ lib/ansible/runner/action_plugins/virt_guest.py
@@ -33,7 +33,7 @@ class ActionModule(object):
 
         # template the source data locally & transfer
         try:
-            xmldata = utils.template_from_file(self.runner.basedir, src, inject)
+            xmldata = utils.template.template_from_file(self.runner.basedir, src, inject)
         except Exception, e:
             result = dict(failed=True, msg='%s (%s)' % (e, src))
             return ReturnData(conn=conn, comm_ok=False, result=result)
